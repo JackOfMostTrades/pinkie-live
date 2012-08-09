7470bea17a419adbb17ed32b704e916b82a8272e
Ian Haken <ianhaken@gmail.com>
2012-08-09 01:48:28 -0700

    Added credits to settings menu.

--- PinkieLive/gen/com/derpfish/pinkielive/R.java
+++ PinkieLive/gen/com/derpfish/pinkielive/R.java
@@ -9,16 +9,25 @@ package com.derpfish.pinkielive;
 
 public final class R {
     public static final class array {
-        public static final int ponyNames=0x7f040000;
-        public static final int ponyValues=0x7f040001;
+        public static final int ponyNames=0x7f050000;
+        public static final int ponyValues=0x7f050001;
     }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int credits_textview=0x7f070000;
+    }
+    public static final class layout {
+        public static final int credits_layout=0x7f030000;
+    }
+    public static final class string {
+        public static final int credits_string=0x7f060000;
+    }
     public static final class xml {
-        public static final int livewallpaper=0x7f030000;
-        public static final int livewallpaper_settings=0x7f030001;
+        public static final int livewallpaper=0x7f040000;
+        public static final int livewallpaper_settings=0x7f040001;
     }
 }
---
+++ PinkieLive/src/com/derpfish/pinkielive/PinkieLiveCredits.java
@@ -0,0 +1,45 @@
+package com.derpfish.pinkielive;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class PinkieLiveCredits extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits_layout);
+        
+        TextView tv = (TextView) findViewById(R.id.credits_textview);
+        tv.setText(Html.fromHtml(getString(R.string.credits_string)));
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+}
